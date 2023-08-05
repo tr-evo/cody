5c15689f63b2ada6ab360a3912642c968cd99404
michaelgau <6x24ylsGaum>
2023-08-05 10:56:05 +0200

    added allowed hosts

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  devServer: {
+    open: process.platform === 'darwin',
+    host: [
+      'localhost',
+      '127.0.0.1',
+      'http://cody.myresearchprocess.com/',
+    ],
+    port: 62041, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false,
+  },
 }
